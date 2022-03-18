a654161ac19b20b3351f81ca1cfa0aaff30eb9e3
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-03-18 16:29:20 +0100

    added css

--- js/main.js
+++ js/main.js
@@ -1,20 +1,7 @@
 const app = new Vue({
     el: "#app",
     data: {
-        todoList: [
-            {
-                text : "Buy butter",
-                done : false
-            },
-            {
-                text : "Buy flower",
-                done : false
-            },
-            {
-                text : "Buy salad",
-                done : false
-            }
-        ],
+        todoList: [ ],
         newListItem: ""
     },
     methods: {
