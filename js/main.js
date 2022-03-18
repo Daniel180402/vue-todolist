7914bbd2870ce8c24c504f1250bc91d6a10c778d
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-03-18 15:08:14 +0100

    initalization vue todo list

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        todoList: [
+            {
+                text : "Buy butter",
+                done : false
+            },
+            {
+                text : "Buy flower",
+                done : false
+            },
+            {
+                text : "Buy salad",
+                done : false
+            }
+        ]
+    },
+    methods: {
+
+    }
+});
